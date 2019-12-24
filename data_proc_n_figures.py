a7ddbc3c5c9f9884a7ab1e583f4002ab73aa1956
amchagas <andremaia.chagas@gmail.com>
2019-12-24 02:01:53 +0000

    update database, figures and code

--- data_proc_n_figures.py
+++ data_proc_n_figures.py
@@ -13,6 +13,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource
 databaseLoc = "/home/andre/repositories/own/OSH_papers_DB/"
 #databaseName = "zotero.sqlite"
 csvName = "hardware.csv"
@@ -23,7 +26,7 @@ os.chdir(databaseLoc)
 fid = pd.read_csv(databaseLoc+csvName)
 
 ## clean up csv table
-remove = ["Key","Call Number","Extra","Notes", "File Attachments",
+remove = ["Key","Call Number","Notes", "File Attachments",
           "Link Attachments","Series Editor",
           "Translator","	Contributor","Attorney Agent","Book Author",
           "Cast Member","Commenter""Composer","Cosponsor","Counsel",
@@ -42,20 +45,54 @@ for item in remove:
     if item in fid.keys():
         fid.pop(item)
 
+
+#index of publications (the dataset also has webpages, conference papers, etc)
+pubIndex = fid[fid["Item Type"]=="journalArticle"].index
+
+#generate another pandas dataframe with only publications
+onlyPubs = fid.loc[pubIndex]
+
+
 #publication year histogram
-pubYear = fid['Publication Year']
+#pubYear = pd.DataFrame(data=fid['Publication Year'].values,columns=["year"],dtype=int)
 
-sns.distplot(pubYear.dropna(),
-             rug=False,kde=False,
-             bins=range(2007,2021))
+#rearrange data to be sorted by publication year
+fid = fid.sort_values("Publication Year")
+label1 = fid["Publication Year"]
+label1 = label1.dropna()
+label1 = label1.astype("int64")
+label1 = label1.unique()
+
+fig1 = sns.catplot(x="Publication Year", kind="count",  data=fid,color="r")
+fig1.set_xticklabels(label1)
+
+#set aside only journal articles
+pubIndex = fid[fid["Item Type"]=="journalArticle"].index
+onlyPub = fid.loc[pubIndex]
+onlyPub = onlyPub.sort_values("Publication Title")
 
 
-#journal and preprint histogram
-allPub = fid["Publication Title"]
-allPub.hist()
+
+
+fig2 = sns.catplot(x="Publication Title", kind="count", data=onlyPub,color="r");
+fig2.set_xticklabels(rotation=90)
+
+fig2.savefig("/home/andre/Desktop/test.png")
+
+
 
 #only preprints
 
+#fid.columns.tolist()
 
+#columns = fid.columns.tolist()
+#source =  ColumnDataSource(fid["Publication Year"])
+#journals = source.data['Publication Title'].tolist()
 
+#number of publications fby year
+#fid["Publication Year"].hist(bins=range(2007,2021))
+#sns.distplot(fid["Publication Title"].dropna(),bins=range(2007,2021),
+#             kde=False,axlabel="year",
+#             vertical=False, norm_hist=False)
 
+#fid["Publication Title"].dropna().hist()
