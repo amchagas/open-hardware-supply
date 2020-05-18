a050c49acc8ed089be4d7acc31e23992e990f379
amchagas <andremaia.chagas@gmail.com>
2020-05-18 01:14:28 +0100

    add initial code to mine pdf from Matias Andina

---
+++ code/test.py
@@ -0,0 +1,22 @@
+#installed with 'conda install pdf-miner.six'
+
+
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+output_string = StringIO()
+
+with open('kumbol_2018.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+        print(["git" in value for value in output_string.getvalue().split("\n")])
