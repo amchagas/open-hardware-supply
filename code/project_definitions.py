6e7c01ec2bc055780fdc012224c189f9df2adaca
Ale Abdo <abdo@member.fsf.org>
2021-02-07 02:29:08 +0100

    Rename load_bibs to data_etl and improve it, add project_definitions.py, replace data with json, correct plos-items.csv

---
+++ code/project_definitions.py
@@ -0,0 +1,48 @@
+#! /bin/env python3
+
+from pathlib import Path
+import pandas as pd
+
+# Project folder structure
+
+baseDir = Path("..")
+dataSourceDir = baseDir / 'data' / 'sources'
+dataOutDir = baseDir / 'data' / 'derived'
+figDir = baseDir / 'figures'
+
+# Project files shared across notebooks
+
+allDataFile = dataOutDir / 'allData.json.xz'
+
+# Project-wide functions
+
+def store_data(dataFrame, dataFile):
+    dataFrame.to_json(dataFile, orient='table')
+
+
+def load_data(dataFile):
+    return pd.read_json(dataFile, orient="table")
+
+
+def build_query():
+    nouns = [
+        'equipment',
+        'hardware',
+        'labware',
+    ]
+    adjectives = [
+        'open',
+        'open source',
+        'open science',
+        'frugal',
+        #'low cost',
+    ]
+    phrases = [
+        ' '.join([a, n]) for a in adjectives for n in nouns
+    ]
+    phrases.extend([
+        "free hardware and software",
+    ])
+    query = " OR ".join(f'"{x}"' for x in phrases)
+
+    return query
