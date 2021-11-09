85ed6211f6dd98b8cdb5ccea7b196b301e82bbc3
Ale Abdo <abdo@member.fsf.org>
2021-11-09 14:40:27 +0100

    Better criteria for querying and matching records from scraped data

--- code/get_wos_records_from_api.py
+++ code/get_wos_records_from_api.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import json
 import httpx
 import re
+import time
+from jellyfish import damerau_levenshtein_distance as edit_distance
+from tqdm.auto import tqdm
 
 WOS_API = "https://wos-api.clarivate.com/api/wos"
 DATA_DIR = Path("/home/eris/Projects/gosh/open-hardware-supply/data/scrapy")
@@ -23,25 +26,76 @@ DATA_FILES = [
     ]
 ]
 
+DEBUG_LOG = True
+
 
 class TitlesToRecords:
     def __init__(self, api_key, target_file, source_files=DATA_FILES):
         self.api_key = api_key
         self.source_files = [Path(file) for file in source_files]
         self.target_file = Path(target_file)
+        self.normalize_rex = re.compile(r"\W+")
+        self.query_fields = ["title", "pubyear", "first_author"]
+        self._debug_log = []
+        self._last_title = None
 
-    def get_titles(self):
-        for file in self.source_files:
-            for line in file.open().readlines():
-                data = json.loads(line)
-                yield data["title"]
+    def normalize_text(self, text):
+        return self.normalize_rex.sub(" ", text).strip().lower()
 
-    def get_query(self, title):
-        title = title.replace('"', "")
-        return f'TI=("{title}")'
+    def debug_log(self, kind, data):
+        if DEBUG_LOG:
+            self._debug_log.append((kind, data))
 
-    def get_record(self, title):
-        query = self.get_query(title)
+    def scraped_data(self):
+        skipping = bool(self._last_title)
+        for file in tqdm(self.source_files):
+            with file.open() as f:
+                total = len(list(f.readlines()))
+            for line in tqdm(file.open().readlines(), total=total, desc=" "):
+                scraped = json.loads(line)
+                data = {}
+
+                data["title"] = self.normalize_text(scraped["title"])
+
+                data["first_author"] = (
+                    scraped["publishedData"].split("-")[0].split(",")[0].split()[-1]
+                )
+
+                if pubyear := re.search(r"\b\d{4}\b", scraped["publishedData"]):
+                    data["pubyear"] = int(pubyear.group())
+                else:
+                    self.debug_log("Missing year", scraped)
+
+                data["_line"] = line
+
+                # DEBUG
+                if "pubyear" in data:
+                    continue
+                # /DEBUG
+
+                if skipping and data["title"] == self._last_title:
+                    skipping = False
+                    tqdm.write(f"Starting from: {self._last_title}")
+                if skipping:
+                    continue
+
+                yield data
+
+    def build_query(self, data):
+        tag = {"title": "TI", "first_author": "AU", "pubyear": "PY"}
+        query = [
+            f"{tag[key]}=({escaped_value})"
+            for key, value in data.items()
+            for escaped_value in [
+                " ".join(f'"{x}"' for x in value.split()) if key == "title" else value,
+            ]
+            if key in self.query_fields
+        ]
+        self.debug_log("Query", query)
+        return " AND ".join(query)
+
+    def get_records_from_scraped_data(self, data):
+        query = self.build_query(data)
         headers = {
             "X-ApiKey": self.api_key,
         }
@@ -52,43 +106,78 @@ class TitlesToRecords:
             "firstRecord": 1,
         }
         r = httpx.get(WOS_API, params=params, headers=headers)
-        rec = None
-        try:
-            rec = r.json()["Data"]["Records"]["records"]["REC"]
-            self.check_record(rec, title)
-            rec = rec[0]
-        except Exception as e:
-            rec = None
-            print(f"\nFailed query: {type(e)}: {e}")
-            print(f"\nParameters: {params}")
-            print(f"\nText: {r.text}")
+        recs = r.json()["Data"]["Records"]["records"]
+        if recs:
+            recs = recs["REC"]
+        else:
+            recs = []
+            self.debug_log("Failed query", {"Parameters": params, "Text": r.text})
+        return recs
 
-        return rec
+    def check_record(self, recs, data):
+        for rec in recs:
+            debug = {}
 
-    def check_record(self, rec, title):
-        if len(rec) > 1:
-            raise ValueError("Multiple results!")
-        rec = rec[0]
-        comp_rex = re.compile(r"\W+")
-        (r_title,) = [
-            x
-            for x in rec["static_data"]["summary"]["titles"]["title"]
-            if x["type"] == "item"
-        ]
-        r_title = r_title["content"]
-        if comp_rex.sub(" ", r_title) != comp_rex.sub(" ", title):
-            raise ValueError("No matching title!")
+            (rec_title,) = [
+                x
+                for x in rec["static_data"]["summary"]["titles"]["title"]
+                if x["type"] == "item"
+            ]
+            rec_title = self.normalize_text(rec_title["content"])
+            data_title = data["title"]
+            title_distance = edit_distance(rec_title, data_title)
+            if title_distance > 5:
+                continue
+            elif title_distance > 0:
+                debug["Bad title"] = (rec_title, data_title)
+            else:
+                debug["Good title"] = data_title
 
-    def get_records(self):
+            rec_authors = [
+                x.get("last_name", x["full_name"].split(",")[0]).strip()
+                for x in rec["static_data"]["summary"]["names"]["name"]
+                if "last_name" in x or "full_name" in x
+            ]
+            author_distance = min(
+                edit_distance(data["first_author"], author) for author in rec_authors
+            )
+            if author_distance > 2:
+                continue
+            elif author_distance > 0:
+                debug["Bad author"] = (rec_authors, data["first_author"])
+            else:
+                debug["Good author"] = data["first_author"]
+
+            if "pubyear" in data:
+                rec_pubyear = rec["static_data"]["summary"]["pub_info"]["pubyear"]
+                pubyear_distance = abs(rec_pubyear - data["pubyear"])
+                if pubyear_distance > 1:
+                    continue
+                elif pubyear_distance > 0:
+                    debug["Bad pubyear"] = (rec_pubyear, data["pubyear"])
+                else:
+                    debug["Good pubyear"] = data["pubyear"]
+
+            if any(x.startswith("Bad") for x in debug):
+                for key, value in debug.items():
+                    self.debug_log(key, value)
+            return rec
+
+        return None
+
+    def dump_records(self):
         self.target_file.touch()
         with open(self.target_file, "r") as tf:
-            records = [json.loads(x)["title"] for x in tf.readlines()]
+            records = [tuple(json.loads(x)["scraped"]) for x in tf.readlines()]
         with open(self.target_file, "a") as tf:
-            for title in self.get_titles():
-                if title not in records:
-                    rec = self.get_record(title)
+            for data in self.scraped_data():
+                rec_key = tuple(data.values())
+                if rec_key not in records:
+                    recs = self.get_records_from_scraped_data(data)
+                    rec = self.check_record(recs, data)
                     if rec:
                         tf.writelines(
-                            [json.dumps({"title": title, "record": rec}), "\n"]
+                            [json.dumps({"scraped": rec_key, "record": rec}), "\n"]
                         )
-                    break
+                    self._last_title = data["title"]
+                    time.sleep(1)
