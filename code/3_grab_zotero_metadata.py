feae41e7f7282eca7be8117aa4ab1f29bdcc2d74
Andre Maia Chagas <andremaia.chagas@gmail.com>
2022-11-09 14:01:11 +0000

    convert ipynb to py

---
+++ code/3_grab_zotero_metadata.py
@@ -0,0 +1,36 @@
+"""
+Small script to load necessary libraries and match google scholar entries to Web of Science entries using WOS api
+"""
+import requests
+import pandas as pd
+from pyzotero import zotero
+import json
+import time
+
+
+dataPath = "../data/derived2/"
+dataFile = "articles_to_review.json"
+articles = pd.read_json(dataPath + dataFile)
+
+dois = list(articles.doi)
+
+url = "http://127.0.0.1:1969/search"  # zotero translator server running locally
+headers = {"content-type": "text/plain", "Accept-Charset": "UTF-8"}
+# r = requests.post(url=url, data=dois[0], headers=headers)
+
+allMeta = list()
+for item in dois:
+    r = requests.post(url=url, data=item, headers=headers)
+    allMeta.append(r.json())
+    r.close()
+
+with open(dataPath + "zotMeta.json", "w") as fid:
+    json.dump(allMeta, fid)
+
+with open(dataPath + "zotMeta.json", "r") as fid:
+    allMeta = json.load(fid)
+# print(data)
+
+for item in allMeta:
+    zot.create_items(item)
+    time.sleep(1)
