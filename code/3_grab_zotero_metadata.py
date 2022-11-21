8e092c4c50c3926107fd28b3481487770c57a1f0
amchagas <andremaia.chagas@gmail.com>
2022-11-21 19:29:51 +0000

    clean up coding

--- code/3_grab_zotero_metadata.py
+++ code/3_grab_zotero_metadata.py
@@ -11,8 +11,10 @@ import time
 dataPath = "../data/derived2/"
 dataFile = "upwData.json"
 articles = pd.read_json(dataPath + dataFile)
+#idx = set(articles.doi)
 
-dois = list(set(articles.doi))
+#articles = articles[articles.title]
+dois = list(articles.doi)
 ##to run zotero translator, one needs to have docker installed and run it so (if one is using command line):
 #docker pull zotero/translation-server
 #docker run -d -p 1969:1969 --rm --name translation-server zotero/translation-server
@@ -23,20 +25,24 @@ headers = {"content-type": "text/plain", "Accept-Charset": "UTF-8"}
 # r = requests.post(url=url, data=dois[0], headers=headers)
 
 
-allMeta = list()
-for item in dois:
-    r = requests.post(url=url, data=item, headers=headers)
-    allMeta.append(r.json())
-    r.close()
+#allMeta = list()
+#for item in dois:
+#    try:
+#        r = requests.post(url=url, data=item, headers=headers)
+#        allMeta.append(r.json())
+#        r.close()
+#    except:
+#        allMeta.append("None")
 
-with open(dataPath + "zotMeta.json", "w") as fid:
-    json.dump(allMeta, fid)
+#with open(dataPath + "zotMeta.json", "w") as fid:
+#    json.dump(allMeta, fid)
 
 
 
 with open(dataPath + "zotMeta.json", "r") as fid:
     allMeta = json.load(fid)
-# print(data)
+ 
+ #print(data)
 
 with open("zotero_api","r") as fid:
     key = fid.readline()
@@ -45,8 +51,8 @@ zot = zotero.Zotero(library_id=4851522,library_type='group', api_key=key)
 
 index=0
 for item in allMeta:
-    index=index+1
-    zot.create_items(item)
-    time.sleep(1)
-    print(index)
-
+    if item !="None":
+        index=index+1
+        zot.create_items(item)
+        time.sleep(1)
+        print(index)
