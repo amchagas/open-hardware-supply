4931c06b8eb6167d2f36d81ee045bba282762f90
Ale Abdo <abdo@member.fsf.org>
2023-03-14 17:03:45 +0100

    0_collect_gscholar_data: facilitate restarting

--- code/0_collect_gscholar_data.py
+++ code/0_collect_gscholar_data.py
@@ -20,19 +20,22 @@ def get_full_query(queries):
     return " OR ".join(queries)
 
 
-def store_pubs(query, year_low=None, year_high=None, out_path=None, **kwargs):
-    start_index = kwargs.pop("start_index", 0)
-    search_pubs_args = {
-        "year_low": year_low,
-        "year_high": year_high,
-        **kwargs,
-        "start_index": start_index,
-    }
-    result_iter = scholarly.search_pubs(query, **search_pubs_args)
+def store_pubs(
+    query, out_path=None, year_low=None, year_high=None, start_index=0, **kwargs
+):
+    search_pubs_args = {"year_low": year_low, "year_high": year_high, **kwargs}
+    result_iter = scholarly.search_pubs(
+        query, **search_pubs_args, start_index=start_index
+    )
     results = []
     with (Path(out_path).open("a") if out_path else nullcontext()) as f:
-        for item in result_iter:
-            results.append(item)
-            if f is not None:
-                f.write(json.dumps(item) + "\n")
+        try:
+            for n, item in enumerate(result_iter):
+                results.append(item)
+                if f is not None:
+                    f.write(json.dumps(item) + "\n")
+            n += 1
+        finally:
+            print(f"Start: {start_index}. Step: {n}. Restart: {start_index + n}.")
+
     return results
