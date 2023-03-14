6a91257b5712b5f1c7faead6f5db0689f0dec30c
Ale Abdo <abdo@member.fsf.org>
2023-03-14 23:16:27 +0100

    Add QueryBuilder and store_all() for years/months slicing

--- code/0_collect_gscholar_data.py
+++ code/0_collect_gscholar_data.py
@@ -1,10 +1,13 @@
+import calendar
 from contextlib import nullcontext
 import json
 from pathlib import Path
 
 from scholarly import scholarly
 
-terms = [
+# Keep it simple and close to the essential terms,
+# and so in the scope of claiming OSH qualities
+TERMS = (
     "open hardware",
     "open source hardware",
     "opensource hardware",
@@ -12,25 +15,54 @@ terms = [
     "open scientific hardware",
     # 'open source instrument',
     # 'open source instrumentation',
-]
-queries = [f'"{term}"' for term in terms]
+)
+
+# 2005 → Arduino, Make, Git, ...
+# full_query [< 2005]: 615 results
+# full_query [>=2000 & < 2005]: 335 results
+YEARS = tuple(range(2005, 2023))
 
 
-def get_full_query(queries):
-    return " OR ".join(queries)
+# Months in current locale (English if not specified)
+MONTHS = tuple(calendar.month_name[m] for m in range(1, 13))
 
 
-def store_pubs(
-    query, out_path=None, year_low=None, year_high=None, start_index=0, **kwargs
-):
+class QueryBuilder:
+    @staticmethod
+    def quote(x):
+        return f'"{x}"'
+
+    @staticmethod
+    def or_(xs):
+        return "(" + " OR ".join(xs) + ")"
+
+    @staticmethod
+    def not_(x):
+        """NOTs must go at the end and outside nontrivial parenthesis"""
+        return f"-{x}"
+
+    @staticmethod
+    def and_(xs):
+        return "(" + " ".join(xs) + ")"
+
+
+def get_full_query(terms):
+    return QueryBuilder.or_(QueryBuilder.quote(term) for term in terms)
+
+
+def get_pubs(query, year_low=None, year_high=None, start_index=0, **kwargs):
     search_pubs_args = {"year_low": year_low, "year_high": year_high, **kwargs}
-    result_iter = scholarly.search_pubs(
-        query, **search_pubs_args, start_index=start_index
-    )
+    pubs = scholarly.search_pubs(query, **search_pubs_args, start_index=start_index)
+    pubs.start_index = start_index
+    return pubs
+
+
+def store_pubs(pubs, out_path=None):
+    start_index = pubs.start_index
     results = []
     with (Path(out_path).open("a") if out_path else nullcontext()) as f:
         try:
-            for n, item in enumerate(result_iter):
+            for n, item in enumerate(pubs):
                 results.append(item)
                 if f is not None:
                     f.write(json.dumps(item) + "\n")
@@ -39,3 +71,30 @@ def store_pubs(
             print(f"Start: {start_index}. Step: {n}. Restart: {start_index + n}.")
 
     return results
+
+
+def store_all(out_dir=None):
+    out_dir = Path(out_dir)
+    for term in TERMS:
+        for year in YEARS:
+            base_query = QueryBuilder.quote(term)
+            query_args = {
+                "query": base_query,
+                "year_low": year,
+                "year_hight": year,
+            }
+            pubs = get_pubs(query_args)
+            if pubs.total_results < 900:
+                store_pubs(pubs, out_path=out_dir / f"{term} - {year}")
+                continue
+            for month in MONTHS:
+                query_args["query"] = QueryBuilder.and_(base_query, month)
+                pubs = get_pubs(query_args)
+                if pubs.total_results < 900:
+                    store_pubs(pubs, out_path=out_dir / f"{term} - {year} - {month}")
+            query_args["query"] = QueryBuilder.and_(
+                base_query, *(QueryBuilder.not_(month) for month in MONTHS)
+            )
+            pubs = get_pubs(query_args)
+            if pubs.total_results < 900:
+                store_pubs(pubs, out_path=out_dir / f"{term} - {year} - {month}")
