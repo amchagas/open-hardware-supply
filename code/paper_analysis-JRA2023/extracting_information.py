c6a335a0eb81cc3aeae11b792165a29bbe5e2eb8
Anton <sir.anton0@gmail.com>
2023-07-27 14:08:03 +0100

    Added function for links searching

--- code/paper_analysis-JRA2023/extracting_information.py
+++ code/paper_analysis-JRA2023/extracting_information.py
@@ -4,20 +4,20 @@ import pandas as pd
 import csv
 import re
 
-df = pd.read_csv('open_hardware_database.csv')
-directory = r"Downloaded_pdfs"
+# df = pd.read_csv('open_hardware_database.csv')
+# directory = r"Downloaded_pdfs"
 
 # While using on my own PC
-# df = pd.read_csv(r"C:\Users\Anton\Desktop\JRA\open_hardware_database.csv")
-# directory = r"C:\Users\Anton\Desktop\JRA\Downloaded_pdfs"
+df = pd.read_csv(r"C:\Users\siran\Desktop\JRA\open_hardware_database.csv")
+directory = r"C:\Users\siran\Desktop\JRA\Downloaded_pdfs"
 
 output_csv_path = r"output.csv"
 section_keywords = {"abstract", "introduction", "reults",
                     "method", "methods", "conclusion", "discussion", "references"}
-open_hardware_variation = {"open hardware", "open_hardware"}
+open_hardware_variation = {"open hardware", "open_hardware", "open-hardware"}
 
-
-def section_keywords_searching(text, line_id, sections):
+# Searching for sections
+def section_keywords_searching(text, line_id : str, sections : set):
     while line_id >= 0:
         line = text[line_id].strip()
         if any(element in line.lower() for element in section_keywords) and len(line) < 40 and re.match("([A-Z]|[0-9])", line):
@@ -27,15 +27,25 @@ def section_keywords_searching(text, line_id, sections):
         line_id -= 1
     return False
 
+# Searching for hyperlinks
+def hyperlinks_searching(page, include : set, exclude : set):
+    if '/Annots' in page:
+                for a in page['/Annots']:
+                    obj = a.get_object()
+                    if '/A' in obj.keys() and '/URI' in obj['/A'].keys():
+                        uri = obj['/A']['/URI']
+                        if any(element in uri for element in include) and not any(element in uri for element in exclude):
+                            return obj['/A']['/URI']
+    else:
+         return None
 
-for filename in os.listdir(directory)[1:10]:
+
+for filename in os.listdir(directory)[1:15]:
     entry = df.loc[df['Key'] == filename[:-4]]
-    csv_entry = [entry['Key'].values[0],
-                 entry['Title'].values[0], entry['DOI'].values[0]]
+    csv_entry = [entry['Key'].values[0], entry['Title'].values[0], entry['DOI'].values[0]]
     links = set()
     sections = set()
     f = os.path.join(directory, filename)
-    print(filename)
     with open(f, 'rb') as pdf_file:
         pdf = PyPDF2.PdfReader(pdf_file)
         for page_id, page in enumerate(pdf.pages):
@@ -50,17 +60,12 @@ for filename in os.listdir(directory)[1:10]:
                     current_text = text
                     while current_page_id >= 0 and not section_keywords_searching(current_text, current_line_id, sections):
                         current_page_id -= 1
-                        current_text = pdf.pages[current_page_id].extract_text(
-                        ).splitlines()
+                        current_text = pdf.pages[current_page_id].extract_text().splitlines()
                         current_line_id = len(current_text) - 1
-
-            # Searching for hyperlinks
-            if '/Annots' in page:
-                for a in page['/Annots']:
-                    obj = a.get_object()
-                    if '/A' in obj.keys() and '/URI' in obj['/A'].keys():
-                        if "doi" not in obj['/A']['/URI']:
-                            links.add(obj['/A']['/URI'])
+            
+            # Adding links
+            uri = hyperlinks_searching(page, {"mendeley", "osf", "github", "gitlab", ".zip"}, {"orchid", "nih", "doi"})
+            if uri != None: links.add(uri)
 
     # Appending an entry to a CSV file named Output.csv
     csv_entry.append(sections) if sections != set() else csv_entry.append(None)
