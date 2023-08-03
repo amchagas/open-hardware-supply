e65b5dba49a240c4b91bdd86bdd6f179bd5a1a03
Anton <sir.anton0@gmail.com>
2023-08-03 14:57:34 +0100

    Added requrest for inputs in both files; scoring system for finding sections

--- code/paper_analysis-JRA2023/downloading_pdfs.py
+++ code/paper_analysis-JRA2023/downloading_pdfs.py
@@ -3,44 +3,48 @@ import requests
 import csv
 from scholarly import scholarly
 
-# Output directory for all downloaded pdfs
-output_directory = r"Downloaded_pdfs"
+def main(output, database, email):
+    # Function to download a PDF file. Takes the url of a PDF file and fetches a get request,
+    # then writes the response to the file that uses Zotero ID as its name
+    def download_pdf(pdf_url, publication_key):
+        response = requests.get(pdf_url)
+        pdf_path = os.path.join(output, f"{publication_key}.pdf")
+        with open(pdf_path, "wb") as f:
+            f.write(response.content)
+        print(f"Downloaded PDF for publication: {publication_key}")
 
-# Function to download a PDF file. Takes the url of a PDF file and fetches a get request,
-# then writes the response to the file that uses Zotero ID as its name
-def download_pdf(pdf_url, publication_key):
-    response = requests.get(pdf_url)
-    pdf_path = os.path.join(output_directory, f"{publication_key}.pdf")
-    with open(pdf_path, "wb") as f:
-        f.write(response.content)
-    print(f"Downloaded PDF for publication: {publication_key}")
+    # Opens a CSV file that contains information about all PDFs that need to be downloaded
+    # and appends their Zotero ID "row[0]" and title of the publication "row[4]" to a list
+    # called "article_keys_and_titles"
+    article_keys_and_titles = []
+    with open(database, 'r', encoding="utf-8") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            article_keys_and_titles.append([row[0], row[4]])
 
-# Opens a CSV file that contains information about all PDFs that need to be downloaded
-# and appends their Zotero ID "row[0]" and title of the publication "row[4]" to a list
-# called "article_keys_and_titles"
-article_keys_and_titles = []
-with open('open_hardware_database.csv', 'r', encoding="utf-8") as csvfile:
-    csv_reader = csv.reader(csvfile)
-    for row in csv_reader:
-        article_keys_and_titles.append([row[0], row[4]])
+    # Checks if a PDF file is already downloaded.
+    # If it is - does nothing, if not - sends a get request to Unpaywall API,
+    # querying for the publication with the specified title name.
+    # It then attempts to download the PDF file using the "url_for_pdf" provided by the response 
+    downloaded_articles = [os.path.splitext(filename)[0] for filename in os.listdir(output)]
+    for article_key_and_title in article_keys_and_titles:
+        key = article_key_and_title[0]
+        title = article_key_and_title[1]
+        if key not in downloaded_articles: 
+            url = f"https://api.unpaywall.org/v2/search?query={title}&email={email}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                result = response.json()
+                try:
+                    pdf_url = result["results"][0]["response"]["best_oa_location"]["url_for_pdf"]
+                    download_pdf(pdf_url, key)
+                except:
+                    print(f"Couldn't download from Unpaywall. Using scholarly: {key}")
+            else:
+                print(f"No data found for publication: {key}")
 
-# Checks if a PDF file is already downloaded.
-# If it is - does nothing, if not - sends a get request to Unpaywall API,
-# querying for the publication with the specified title name.
-# It then attempts to download the PDF file using the "url_for_pdf" provided by the response 
-downloaded_articles = [os.path.splitext(filename)[0] for filename in os.listdir(output_directory)]
-for article_key_and_title in article_keys_and_titles:
-    key = article_key_and_title[0]
-    title = article_key_and_title[1]
-    if key not in downloaded_articles: 
-        url = f"https://api.unpaywall.org/v2/search?query={title}&email=av337@sussex.ac.uk"
-        response = requests.get(url)
-        if response.status_code == 200:
-            result = response.json()
-            try:
-                pdf_url = result["results"][0]["response"]["best_oa_location"]["url_for_pdf"]
-                download_pdf(pdf_url, key)
-            except:
-                print(f"Couldn't download from Unpaywall. Using scholarly: {key}")
-        else:
-            print(f"No data found for publication: {key}")
+if __name__ == "__main__":
+    output = input("Path to the output directory: ")
+    database = input("Path to the downloaded database: ")
+    email = input("Your email for Unpaywall API: ")
+    main(output, database, email)
--- code/paper_analysis-JRA2023/extracting_information.py
+++ code/paper_analysis-JRA2023/extracting_information.py
@@ -1,75 +1,92 @@
 import os
-import PyPDF2
 import pandas as pd
-import csv
 import re
+import PyPDF2
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer, LTChar, LTTextLine, LTComponent
 
-# df = pd.read_csv('open_hardware_database.csv')
-# directory = r"Downloaded_pdfs"
-
-# While using on my own PC
-df = pd.read_csv(r"C:\Users\siran\Desktop\JRA\open_hardware_database.csv")
-directory = r"C:\Users\siran\Desktop\JRA\Downloaded_pdfs"
-
-output_csv_path = r"output.csv"
-section_keywords = {"abstract", "introduction", "reults",
-                    "method", "methods", "conclusion", "discussion", "references"}
+section_keywords = {"abstract", "introduction", "reults", "method", "conclusion", "discussion", "references"}
 open_hardware_variation = {"open hardware", "open_hardware", "open-hardware"}
 
 # Searching for sections
-def section_keywords_searching(text, line_id : str, sections : set):
-    while line_id >= 0:
-        line = text[line_id].strip()
-        if any(element in line.lower() for element in section_keywords) and len(line) < 40 and re.match("([A-Z]|[0-9])", line):
-            # print(line)
-            sections.add(line)
-            return True
-        line_id -= 1
-    return False
+def section_keywords_searching(pages: list[LTComponent], page_id : int, element_id : int, found_font_name : str):
+    if page_id >= 0:
+        page = list(pages[page_id])
+        while element_id >= 0:
+            if isinstance(page[element_id], LTTextContainer):
+                score = 0
+                line = page[element_id]._objs[0]
+                text = line.get_text()
+                first_char_font_name = [char.fontname for char in line if isinstance(char, LTChar)][0]
+                if first_char_font_name != found_font_name:
+                    score += 1
+                if re.match("[A-Z]", text):
+                    score += 1
+                if any(element in text.lower() for element in section_keywords):
+                    score += 2
+                if re.match("([0-9]\.)", text):
+                    score += 3
+                if "," in text:
+                    score -= 2
+                if len(text) > 40:
+                    score -= 1
+                if re.search('[a-zA-Z]', text) == None:
+                    score -= 5
+                if score > 2:
+                    return text[:-1]
+            element_id -= 1
+        return section_keywords_searching(pages, page_id - 1, len(pages[page_id - 1]) - 1, found_font_name)
+    return "Error: No sections found"
 
 # Searching for hyperlinks
 def hyperlinks_searching(page, include : set, exclude : set):
     if '/Annots' in page:
-                for a in page['/Annots']:
-                    obj = a.get_object()
-                    if '/A' in obj.keys() and '/URI' in obj['/A'].keys():
-                        uri = obj['/A']['/URI']
-                        if any(element in uri for element in include) and not any(element in uri for element in exclude):
-                            return obj['/A']['/URI']
+        for a in page['/Annots']:
+            obj = a.get_object()
+            if '/A' in obj.keys() and '/URI' in obj['/A'].keys():
+                uri = obj['/A']['/URI']
+                if any(element in uri for element in include) and not any(element in uri for element in exclude):
+                    return obj['/A']['/URI']
     else:
-         return None
+        return None
 
+def main(output_csv, database, downloaded):
 
-for filename in os.listdir(directory)[1:15]:
-    entry = df.loc[df['Key'] == filename[:-4]]
-    csv_entry = [entry['Key'].values[0], entry['Title'].values[0], entry['DOI'].values[0]]
-    links = set()
-    sections = set()
-    f = os.path.join(directory, filename)
-    with open(f, 'rb') as pdf_file:
-        pdf = PyPDF2.PdfReader(pdf_file)
-        for page_id, page in enumerate(pdf.pages):
+    df = pd.read_csv(database)
 
+    for filename in os.listdir(downloaded)[4:10]:
+        entry = df.loc[df['Key'] == filename[:-4]]
+        csv_entry = [entry['Key'].values[0], entry['Title'].values[0], entry['DOI'].values[0]]
+        links = set()
+        sections = set()
+        f = os.path.join(downloaded, filename)
+        pages = list(extract_pages(f))
+        for page_id, page in enumerate(pages):
+            page = list(page)
             # Searching for keywords
-            text = page.extract_text().splitlines()
-            for line_id in range(len(text)):
-                if any(element in text[line_id].lower() for element in open_hardware_variation):
-                    # print(page_id)
-                    current_page_id = page_id
-                    current_line_id = line_id
-                    current_text = text
-                    while current_page_id >= 0 and not section_keywords_searching(current_text, current_line_id, sections):
-                        current_page_id -= 1
-                        current_text = pdf.pages[current_page_id].extract_text().splitlines()
-                        current_line_id = len(current_text) - 1
-            
+            for element_id, element in enumerate(page):
+                if isinstance(element, LTTextContainer):
+                    text_objects = [object for object in element._objs if isinstance(object, LTTextLine)]
+                    for line in text_objects:
+                        if any(phrase in line.get_text().lower() for phrase in open_hardware_variation):
+                            sections.add(section_keywords_searching(pages, page_id, element_id, [char.fontname for char in line if isinstance(char, LTChar)][0]))
+
             # Adding links
-            uri = hyperlinks_searching(page, {"mendeley", "osf", "github", "gitlab", ".zip"}, {"orchid", "nih", "doi"})
+            reader = PyPDF2.PdfReader(f)
+            page_pdf2 = reader.pages[page_id]
+            uri = hyperlinks_searching(page_pdf2, {"mendeley", "osf", "github", "gitlab", ".zip"}, {"orchid", "nih", "doi"})
             if uri != None: links.add(uri)
 
-    # Appending an entry to a CSV file named Output.csv
-    csv_entry.append(sections) if sections != set() else csv_entry.append(None)
-    csv_entry.append(links) if links != set() else csv_entry.append(None)
-    print(csv_entry)
-    # with open(output_csv_path, 'a', newline='') as output:
-    #     csv.writer(output).writerow(csv_entry)
+        # Appending an entry to a CSV file named Output.csv
+        csv_entry.append(sections) if sections != set() else csv_entry.append(None)
+        csv_entry.append(links) if links != set() else csv_entry.append(None)
+        print(csv_entry)
+        # with open(output_csv, 'a', newline='') as output:
+        #     csv.writer(output).writerow(csv_entry)
+
+
+if __name__ == "__main__":
+    output = input("Path to the output csv file: ")
+    database = input("Path to the downloaded database: ")
+    downloaded = input("Path to the downloaded pdf files: ")
+    main(output, database, downloaded)
